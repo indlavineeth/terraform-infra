962dd9d2cd5c6d51de7b1d5f04e3f1000bade1a0
vineeth <you@example.com>
2022-09-08 12:29:37 +0530

    ec2 added

--- scripts/jenkins-install.sh
+++ scripts/jenkins-install.sh
@@ -6,3 +6,10 @@ sudo yum upgrade
 sudo yum install java-11-openjdk
 sudo yum install jenkins
 sudo systemctl daemon-reload
+
+
+#to install terraform
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
